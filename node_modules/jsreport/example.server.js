eea746590aa0e519ee21b85df5bc7808aed27576
BJR Matos <bjrmatos@gmail.com>
2020-11-10 12:08:01 -0500

    include jsreport in version control

---
+++ node_modules/jsreport/example.server.js
@@ -0,0 +1,13 @@
+const jsreport = require('jsreport')()
+
+if (process.env.JSREPORT_CLI) {
+  module.exports = jsreport
+} else {
+  jsreport.init().then(() => {
+    // running
+  }).catch((e) => {
+    // error during startup
+    console.error(e.stack)
+    process.exit(1)
+  })
+}
