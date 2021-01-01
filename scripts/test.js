15dc83b3e7ab6d4b51c917df56d41c9ebcd92bee
pofider <honza.pofider@seznam.cz>
2021-01-01 23:33:31 +0100

    test justs chrome

--- scripts/test.js
+++ scripts/test.js
@@ -3,36 +3,42 @@ const path = require('path')
 const execSync = require('child_process').execSync
 
 const jsreportPackages = [
-  'jsreport-assets',
+  /* 'jsreport-assets',
   'jsreport-authentication',
   'jsreport-authorization',
-  'jsreport-base',
+  'jsreport-base', */
   'jsreport-core',
-  'jsreport-data',
+  /* 'jsreport-data',
   'jsreport-docx',
   'jsreport-express',
-  'jsreport-fs-store',
+  'jsreport-fs-store', */
   'jsreport-handlebars',
-  'jsreport-child-templates',
+  /* 'jsreport-child-templates', */
   'jsreport-chrome-pdf',
-  'jsreport-jsrender',
+  /* 'jsreport-jsrender',
   'jsreport-pdf-utils',
   'jsreport-scripts',
-  'jsreport-studio',
+  'jsreport-studio', */
   'jsreport-templates'
 ]
 // fs.readdirSync(path.join(__dirname, '../', 'packages'))
 
 for (const pd of jsreportPackages) {
-  console.log('npm i in ' + path.join(__dirname, '../', 'packages', pd))
   fs.rmdirSync(path.join(__dirname, '../', 'packages', pd, 'node_modules'), { recursive: true })
   if (fs.existsSync(path.join(__dirname, '../', 'packages', pd, 'package-lock.json'))) {
     fs.unlinkSync(path.join(__dirname, '../', 'packages', pd, 'package-lock.json'))
   }
-  execSync('npm i', {
-    cwd: path.join(__dirname, '../', 'packages', pd),
-    stdio: 'inherit'
-  })
+  try {
+    execSync('npm i', {
+      cwd: path.join(__dirname, '../', 'packages', pd),
+      stdio: 'inherit'
+    })
+  } catch (e) {
+    execSync('npm i', {
+      cwd: path.join(__dirname, '../', 'packages', pd),
+      stdio: 'inherit'
+    })
+  }
 }
 
 for (const pd of jsreportPackages) {
