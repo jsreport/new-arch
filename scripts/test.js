9fdc8b56396e2219995b26cc55abb73d79cc9a89
pofider <honza.pofider@seznam.cz>
2021-01-01 22:51:17 +0100

    run first install and then  test

--- scripts/test.js
+++ scripts/test.js
@@ -24,15 +24,18 @@ const jsreportPackages = [
 // fs.readdirSync(path.join(__dirname, '../', 'packages'))
 
 for (const pd of jsreportPackages) {
-  console.log('npm i in ' + path.join(__dirname, '../', 'packages', pd))
+  console.log('npm ci in ' + path.join(__dirname, '../', 'packages', pd))
   fs.rmdirSync(path.join(__dirname, '../', 'packages', pd, 'node_modules'), { recursive: true })
   if (fs.existsSync(path.join(__dirname, '../', 'packages', pd, 'package-lock.json'))) {
     fs.unlinkSync(path.join(__dirname, '../', 'packages', pd, 'package-lock.json'))
   }
-  execSync('npm i', {
+  execSync('npm ci', {
     cwd: path.join(__dirname, '../', 'packages', pd),
     stdio: 'inherit'
   })
+}
+
+for (const pd of jsreportPackages) {
   console.log('npm test in ' + path.join(__dirname, '../', 'packages', pd))
   execSync('npm test', {
     cwd: path.join(__dirname, '../', 'packages', pd),
