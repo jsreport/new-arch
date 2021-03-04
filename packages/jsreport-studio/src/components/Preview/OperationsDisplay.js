54e992055d11ced9a527af7b4ef8b01c68aabe4e
BJR Matos <bjrmatos@gmail.com>
2021-03-04 11:59:50 -0500

    add autolayout of nodes in profiler operations display

--- packages/jsreport-studio/src/components/Preview/OperationsDisplay.js
+++ packages/jsreport-studio/src/components/Preview/OperationsDisplay.js
@@ -1,16 +1,40 @@
-import { useCallback, useMemo } from 'react'
+import { useEffect, useRef, useCallback, useMemo } from 'react'
 import classNames from 'classnames'
-import ReactFlow, { Controls } from 'react-flow-renderer'
+import ReactFlow, { Controls, isNode } from 'react-flow-renderer'
+import dagre from 'dagre'
 import styles from './Preview.css'
 
 const OperationsDisplay = (props) => {
   const { activeOperation, operations, errors, onCanvasClick, onOperationClick } = props
+  const graphInstanceRef = useRef(null)
+
+  const onLoad = useCallback((reactFlowInstance) => {
+    graphInstanceRef.current = reactFlowInstance
+  }, [])
 
   const onElementClick = useCallback((ev, element) => {
     onOperationClick({ operation: element.data.operation, error: element.data.error })
   }, [onOperationClick])
 
   const elements = useMemo(() => getElementsFromOperations(operations, errors, activeOperation), [operations, errors, activeOperation])
+  const firstOperation = elements[0]
+  const isCompleted = firstOperation != null ? firstOperation.data.operation.completed : false
+
+  useEffect(() => {
+    if (graphInstanceRef.current == null) {
+      return
+    }
+
+    setTimeout(() => {
+      if (graphInstanceRef.current == null) {
+        return
+      }
+
+      if (isCompleted) {
+        graphInstanceRef.current.fitView()
+      }
+    }, 200)
+  }, [isCompleted])
 
   return (
     <div className={styles.profilerOperations}>
@@ -24,6 +48,8 @@ const OperationsDisplay = (props) => {
         panOnScroll
         selectNodesOnDrag={false}
         onlyRenderVisibleElements={false}
+        defaultZoom={0.8}
+        onLoad={onLoad}
         onElementClick={onElementClick}
         onPaneClick={onCanvasClick}
       >
@@ -35,14 +61,13 @@ const OperationsDisplay = (props) => {
 
 function getElementsFromOperations (operations, errors, activeOperation) {
   const elements = []
+  const defaultPosition = { x: 0, y: 0 }
   let prevElement
   let needsEndNode = false
 
   for (let i = 0; i < operations.length; i++) {
     const operation = operations[i]
     const isActive = operation.id === activeOperation
-    let xValue
-    let yValue
 
     if (operation.previousOperationId != null) {
       const edge = {
@@ -56,14 +81,6 @@ function getElementsFromOperations (operations, errors, activeOperation) {
       elements.push(edge)
     }
 
-    if (i === 0) {
-      xValue = 100
-      yValue = 25
-    } else {
-      xValue = prevElement.position.x
-      yValue = prevElement.position.y + 85
-    }
-
     const nodeClass = classNames(styles.profilerOperationNode, {
       [styles.active]: isActive,
       [styles.running]: !operation.completed && i !== 0
@@ -72,7 +89,7 @@ function getElementsFromOperations (operations, errors, activeOperation) {
     const node = {
       id: operation.id,
       data: { label: operation.name, operation },
-      position: { x: xValue, y: yValue },
+      position: defaultPosition,
       className: nodeClass
     }
 
@@ -95,7 +112,7 @@ function getElementsFromOperations (operations, errors, activeOperation) {
     const endNode = {
       id: `${elements[0].id}-end`,
       data: { label: 'end', error: errors.general },
-      position: { x: lastNode.position.x, y: lastNode.position.y + 85 },
+      position: defaultPosition,
       className: endNodeClass
     }
 
@@ -110,7 +127,38 @@ function getElementsFromOperations (operations, errors, activeOperation) {
     })
   }
 
-  return elements
+  const dagreGraph = new dagre.graphlib.Graph()
+
+  dagreGraph.setDefaultEdgeLabel(() => ({}))
+  dagreGraph.setGraph({ rankdir: 'LR' })
+
+  elements.forEach((el) => {
+    if (isNode(el)) {
+      dagreGraph.setNode(el.id, { width: 150, height: 50 })
+    } else {
+      dagreGraph.setEdge(el.source, el.target)
+    }
+  })
+
+  dagre.layout(dagreGraph)
+
+  return elements.map((el) => {
+    if (isNode(el)) {
+      const nodeWithPosition = dagreGraph.node(el.id)
+
+      el.targetPosition = 'left'
+      el.sourcePosition = 'right'
+
+      // we need this little hack to pass a slighltiy different position
+      // in order to notify react flow about the change
+      el.position = {
+        x: nodeWithPosition.x + Math.random() / 1000,
+        y: nodeWithPosition.y
+      }
+    }
+
+    return el
+  })
 }
 
 export default OperationsDisplay
