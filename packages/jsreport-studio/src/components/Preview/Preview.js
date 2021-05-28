a9599529b7e231cde2f36b91eacf76c59b8febb0
BJR Matos <bjrmatos@gmail.com>
2021-05-28 14:51:59 -0500

    fix studio tests

--- packages/jsreport-studio/src/components/Preview/Preview.js
+++ packages/jsreport-studio/src/components/Preview/Preview.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useCallback } from 'react'
+import React, { useState, useRef, useCallback } from 'react'
 import classNames from 'classnames'
 import Popup from '../common/Popup'
 import styles from './Preview.css'
--- packages/jsreport-studio/test/ui/components/Preview/PreviewTest.js
+++ packages/jsreport-studio/test/ui/components/Preview/PreviewTest.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import should from 'should'
-import { Preview } from '../../../../src/components/Preview/Preview.js'
+import Preview from '../../../../src/components/Preview/Preview'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
@@ -9,6 +9,6 @@ Enzyme.configure({ adapter: new Adapter() })
 // don't find out such kind of component tests very much useful, but contributions welcome
 describe('<Preview />', () => {
   it('calls componentDidMount', () => {
-    should(shallow(<Preview />).text()).containEql('<PreviewDisplay />')
+    should(shallow(<Preview />).find('.block')).have.lengthOf(1)
   })
 })
